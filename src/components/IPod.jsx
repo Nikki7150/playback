5220f18121a25506d84ed9bde957c93ea5230665
Nikki7150 <kanchan.nishka@gmail.com>
2026-06-26 16:51:18 -0700

    added some base code

---
+++ src/components/IPod.jsx
@@ -0,0 +1,12 @@
+import "../styles/ipod.css";
+
+function IPod() {
+    return (
+        <div className="ipod-container">
+            <h1>PlayBack</h1>
+            <div className="ipod"></div>
+        </div>
+    );
+}
+
+export default IPod;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import IPod from "../components/IPod";
+
+function Home() {
+    return (
+        <div className="home">
+            <IPod />
+        </div>
+    );
+}
+
+export default Home;
