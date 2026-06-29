8b8f8571cca248b946e5dfc97ab7e463f7a42afb
Nikki7150 <kanchan.nishka@gmail.com>
2026-06-29 00:05:49 -0700

    added changes asked

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -3,6 +3,13 @@ import "../styles/ipod.css";
 function Screen() {
     return (
         <div className="screen">
+            <h3>PlayBack</h3>
+            <ul className="menu-items">
+                <li>Library</li>
+                <li>Playlists</li>
+                <li>Now Playing</li>
+                <li>Settings</li>
+            </ul>
         </div>
     );
 }
